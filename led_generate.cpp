7254573eee9d6019e13add7ce395265e09a235b0
Drew Masters <dmaster1@lap167.local>
2017-05-01 20:25:27 -0400

    generation code working

--- led_generate.cpp
+++ led_generate.cpp
@@ -2,39 +2,36 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <ctype.h>
 
 using namespace std;
 
 int main(int argc, char **argv){
-	string input, filename, tmp;
+	string input, filename, tmp, output[7];
 	vector <string *> characters;
-	vector <string> fnames;
+	vector <char> fnames;
 	string *tmp_c;
-	int i, j;
+	int i, j, k, l, pos;
 	ifstream infile,tmpfile;
 
-	if (argc !=2) fprintf(stderr,"./generator input_string\n");
+	if (argc !=2) {
+		fprintf(stderr,"./generator input_string\n");
+		exit(1);
+	}
 	input = string(argv[1]);
-	if (input.size() > 100) fprintf(stderr,"input string can only be up to 100 characters\n");
+	if (input.size() > 100) {
+		fprintf(stderr,"input string can only be up to 100 characters\n");
+		exit(1);
+	}
 
 	infile.open("./filenames.txt",ios::in);
 	while(getline(infile,filename)){
 		tmpfile.open("characters/"+filename,ios::in);
 		if (tmpfile.is_open()) {
-			fnames.push_back(filename);
+			fnames.push_back(filename[0]);
 			i=0;
 			tmp_c = new string[7];
-			//for (i=0;i<70;i++)tmp_c[i]=0;
 			while (getline(tmpfile,tmp)){
-				/*for (i=0; i<10; i++){
-					if (tmp[i]=='.') {
-						tmp_c[j]=1;
-					}
-					else {
-						tmp_c[j]=0;
-					}
-					j++;
-				}*/
 				tmp_c[i]=tmp;
 				i++;
 			}
@@ -47,18 +44,26 @@ int main(int argc, char **argv){
 	}
 	infile.close();
 
-	cout << characters.size() << endl;
-
-	for (i=0; i<characters.size();i++){
-		cout << fnames[i] << endl;
-		tmp_c = characters[i];
-		for (j=0; j<7;j++){
-			//for (int k=0; k<10; k++){
-			//	cout << tmp_c[j*10+k];
-			//}
-			//cout << endl;
-			cout << tmp_c[j] << endl;
+	for (i=0; i<7*input.size();i++){
+		j = i/input.size();
+		k = i%input.size();
+	
+		for (l=0; l<fnames.size();l++){
+			if (input[k]!=' '){
+			if (toupper(input[k])==fnames[l]){
+				pos = l;
+				break;
+			}
+			}
+			else{
+				pos = fnames.size()-1;
+			}
 		}
-		cout << endl << endl;
+
+		output[j] = output[j] + characters[pos][j];
+	}
+
+	for (i=0; i<7; i++){
+		cout << output[i] << endl;
 	}
 }
